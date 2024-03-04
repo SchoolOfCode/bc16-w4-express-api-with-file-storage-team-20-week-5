24c284c4ff5e4c904ba0350f16f30cc3a5c0fd4b
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-04 17:13:33 +0000

    added get recipes helper funtion

--- app.js
+++ app.js
@@ -1,62 +1,63 @@
-import express from 'express';
+import express from "express";
 
 import {
-  getRecipes,
-  getRecipeByID,
-  createRecipe,
-  updateRecipeByID,
-  deleteRecipeByID,
-} from './recipes.js';
+	getRecipes,
+	getRecipeByID,
+	createRecipe,
+	updateRecipeByID,
+	deleteRecipeByID,
+} from "./recipes.js";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 // create a get route
 // respond with dummy response
-app.get('/api/recipes', function (req, res) {
-  res.json({ request: 'success' });
+app.get("/api/recipes", async function (req, res) {
+	const recipes = await getRecipes();
+	res.json(recipes);
 });
 
 // create a get by ID route
 // Take ID out of body params
 // respond with dummy response
 
-app.get('/api/recipes/:id', function (req, res) {
-  const extractedID = req.params.id;
-  res.json({ requestedID: extractedID });
+app.get("/api/recipes/:id", function (req, res) {
+	const extractedID = req.params.id;
+	res.json({ requestedID: extractedID });
 });
 
 // create a post request
 // want to add to body with req.body
 // respond in json form with boolean and recipes as payload
 
-app.post('/api/recipes', function (req, res) {
-  const newRecipe = req.body;
-  res.json({ newRecipe: newRecipe, success: true });
+app.post("/api/recipes", function (req, res) {
+	const newRecipe = req.body;
+	res.json({ newRecipe: newRecipe, success: true });
 });
 
 // grab req body
 // grab params id
 
-app.patch('/api/recipes/:id', function (req, res) {
-  const id = req.params.id;
-  const recipeEdit = req.body;
+app.patch("/api/recipes/:id", function (req, res) {
+	const id = req.params.id;
+	const recipeEdit = req.body;
 
-  res.json({ recipeEdit, id: id });
+	res.json({ recipeEdit, id: id });
 });
 
 // create a delete request
 // take id out of body params
 // respond with dummy response
 
-app.delete('/api/recipes/:id', function (req, res) {
-  const extractedID = req.params.id;
-  res.json({ requestedID: extractedID });
+app.delete("/api/recipes/:id", function (req, res) {
+	const extractedID = req.params.id;
+	res.json({ requestedID: extractedID });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -1,10 +1,37 @@
+import { error } from "node:console";
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "recipes.json";
 
+async function readJsonFile(filePath) {
+	try {
+		const jsonData = await fs.readFile(filePath, "utf-8");
+		const data = JSON.parse(jsonData);
+		return data;
+	} catch (error) {
+		console.error("Error reading file", error);
+	}
+}
+
+async function writeJsonFile(filePath, objectToWrite) {
+	try {
+		await fs.writeFile(
+			filePath,
+			JSON.stringify(objectToWrite, null, 2),
+			"utf-8"
+		);
+		return objectToWrite;
+	} catch (error) {
+		console.error(`Error writing file to ${filePath} `, error);
+	}
+}
+
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+	const recipes = await readJsonFile(fileName);
+	return recipes;
+}
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {}
