b92902ecca1c0ce435ebda0a4ffe243b8a789409
Mahdi <mahdisattarnia@hotmail.com>
2024-03-04 14:05:07 +0000

    post request completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 import {
   getRecipes,
@@ -6,27 +6,36 @@ import {
   createRecipe,
   updateRecipeByID,
   deleteRecipeByID,
-} from './recipes.js';
+} from "./recipes.js";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 // create a get route
 // respond with dummy response
-app.get('/api/recipes', function (req, res) {
-  res.json({ request: 'success' });
+app.get("/api/recipes", function (req, res) {
+  res.json({ request: "success" });
 });
 
 // create a get by ID route
 // Take ID out of body params
 // respond with dummy response
 
-app.get('/api/recipes/:id', function (req, res) {
+app.get("/api/recipes/:id", function (req, res) {
   const extractedID = req.params.id;
   res.json({ requestID: extractedID });
+});
+
+// create a post request
+// want to add to body with req.body
+// respond in json form with boolean and recipes as payload
+
+app.post("/api/recipes", function (req, res) {
+  const newRecipe = req.body;
+  res.json({ newRecipe: newRecipe, sucess: true });
 });
 
 app.listen(PORT, () => {
